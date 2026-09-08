8383ff476b46e8c608c8ae48e157d7d31506e2ac
pawel668 <pmacherski@gmail.com>
2026-09-08 22:32:12 +0200

    Add Supabase public config

---
+++ config.js
@@ -0,0 +1,5 @@
+window.MF_CONFIG = {
+  supabaseUrl: "https://zcsvgwtnsxdnaiindjln.supabase.co",
+  supabasePublishableKey: "sb_publishable_5iebWHWeVchQg6YxsMNfpA_YQMuEcb3",
+  authEmail: "mf-dashboard@example.com"
+};
